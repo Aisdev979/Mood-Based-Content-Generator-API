17a18ac284e6b7815d16e17879d361dc01c610b7
Sarahtuns16 <sarahtuns16@gmail.com>
2026-03-03 13:48:50 -0800

    initial commit

---
+++ models/content.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const contentSchema = new mongoose.Schema(
+    {
+     title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    body: {
+        type: String,
+        required: true,
+    },
+
+    type: {
+        type: String,
+        required: true,
+    },
+
+    moods: [
+        {
+            type: String,
+            required: true,
+        },
+    ],
+
+    tags: [
+        {
+            type: String,
+        },
+    ],
+
+    likesCount: {
+        type: Number,
+        default: 0,
+    },
+
+    isApproved: {
+        type: Boolean,
+        default: true,
+    },
+
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+},
+{ timestamps: true }
+);
+
+contentSchema.index({ type: 1, moods: 1});
+
+const Content = mongoose.model("Content", contentSchema);
+
+export default Content;
