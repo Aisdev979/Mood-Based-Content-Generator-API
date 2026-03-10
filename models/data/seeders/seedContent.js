59b7b8662ce39ea184aeaad55c36cc67f5cd7e7b
Sarahtuns16 <sarahtuns16@gmail.com>
2026-03-10 02:16:25 -0700

    Updated seedContent.js to use existing database connection

--- models/data/seeders/seedContent.js
+++ models/data/seeders/seedContent.js
@@ -1,35 +1,36 @@
-import mongoose from "mongoose";
+import connectDB from "./config/db.js"; // use your existing DB connection
 import Content from "./models/content.model.js";
 import User from "./models/user.model.js";
 import sampleContents from "./data/sampleContents.js";
 
 const seedContent = async () => {
-    try {
-     await mongoose.connect(process.env.MONGO_URI);
+  try {
+    // use the existing database connection
+    await connectDB();
 
-     const users = await User.find();
-    
+    const users = await User.find();
+
     const getRandomAuthor = () => {
-    const randomIndex = Math.floor(Math.random() * users.length);
-    return users[randomIndex]._id;
-};
-
-const contentsWithAuthors = sampleContents.map(content => {
-    return {
-    ...content,
-    author: getRandomUserId()
+      const randomIndex = Math.floor(Math.random() * users.length);
+      return users[randomIndex]._id;
     };
-});
 
- await Content.deleteMany();
- await Content.insertMany(contentsWithAuthors);
+    const contentsWithAuthors = sampleContents.map((content) => {
+      return {
+        ...content,
+        author: getRandomAuthor(),
+      };
+    });
 
- console.log("Content Seeded Successfully");
- process.exit();
-} catch (error) {
+    await Content.deleteMany();
+    await Content.insertMany(contentsWithAuthors);
+
+    console.log("Content Seeded Successfully");
+    process.exit();
+  } catch (error) {
     console.error(error);
     process.exit(1);
-}
+  }
 };
 
 seedContent();
